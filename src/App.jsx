e937d763e2a807e564430e17dfb0b8367b06a0af
Jean Fernandes de Macedo <jfmacedo91@gmail.com>
2021-03-10 23:22:04 -0300

    added styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,10 @@
+import styles from './styles/global.scss';
+
 export function App() {
-  return <h1>Hello world!</h1>
+  return (
+    <>
+      <h1>Hello world!</h1>
+      <p>Jean Fernandes de Macedo aqui!</p>
+    </>
+  )
 }
